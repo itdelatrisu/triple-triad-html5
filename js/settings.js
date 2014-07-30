d4eda6e5124887500d7d883201cba8de60c7e165
Jeffrey Han <itdelatrisu@gmail.com>
2014-07-30 16:47:33 -0400

    Added button for changing rules. - Icons taken from "Web0.2ama" icon pack by chrfb: http://chrfb.deviantart.com/art/Web0-2ama-165156124 - CSS taken from http://stackoverflow.com/questions/7577308/.
    Other changes:
    - Fixed issue with restarting if SUDDEN_DEATH is disabled.
    
    Signed-off-by: Jeffrey Han <itdelatrisu@gmail.com>

--- js/settings.js
+++ js/settings.js
@@ -46,3 +46,31 @@ Game.rules = {
 	"ELEMENTAL": true,
 	"SUDDEN_DEATH": true
 };
+
+/**
+ * Updates game rules in popup display.
+ */
+Game.updateRuleDisplay = function() {
+	// add game rules to popup
+	var str = "";
+	for (var rule in Game.rules) {
+		// format rule for display
+		var display = rule.toLowerCase();
+		display = display.replace("_", " ");
+		display = display.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+		display = "<strong>" + display + "</strong>: <span style=color:";
+		display = display + ((Game.rules[rule] === true) ? "#0055FF>ON" : "#FF0000>OFF") + "</span>";
+		display = "<span onclick=Game.toggleRule('" + rule + "')>" + display + "</span><br />";
+
+		str += display;
+	}
+	document.getElementById("rules_popup").innerHTML = str;
+}
+
+/**
+ * Toggles a rule.
+ */
+Game.toggleRule = function(rule) {
+	Game.rules[rule] = !Game.rules[rule];
+	Game.updateRuleDisplay();
+}
